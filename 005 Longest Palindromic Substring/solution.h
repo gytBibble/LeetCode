bde2100b613196019b823e34ba227e80a2830560
gyt <1736103647@qq.com>
2019-08-18 19:31:22 +0800

    005 C++ 2

--- 005 Longest Palindromic Substring/solution.h
+++ 005 Longest Palindromic Substring/solution.h
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 {
 public:
     string longestPalindrome(string s) {
         if(s.size() < 1) return "";
@@ -24,5 +24,33 @@ public:
             right++;
         }
         return right - left - 1; // the length of substring
+    }
+};
+
+class Solution_2 { // Manacher's Algorithm
+public:
+    string longestPalindrome(string s) {
+        // Insert '#'
+        string t = "$#";
+        for (int i = 0; i < s.size(); ++i) {
+            t += s[i];
+            t += "#";
+        }
+        // Process t
+        vector<int> p(t.size(), 0);
+        int mx = 0, id = 0, resLen = 0, resCenter = 0;
+        for (int i = 1; i < t.size(); ++i) {
+            p[i] = mx > i ? min(p[2 * id - i], mx - i) : 1;
+            while (t[i + p[i]] == t[i - p[i]]) ++p[i];
+            if (mx < i + p[i]) {
+                mx = i + p[i]; 
+                id = i;
+            }
+            if (resLen < p[i]) {
+                resLen = p[i];
+                resCenter = i;
+            }
+        }
+        return s.substr((resCenter - resLen) / 2, resLen - 1);
     }
 };
