c27ea0f07d88ae11757ebc43fae32d41ab220787
gyt <1736103647@qq.com>
2019-08-19 16:50:49 +0800

    009 C++ 1

---
+++ 009. Palindrome Number/solution.h
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        long reverse = 0;
+        int temp = x;
+        while(temp) {
+            reverse = reverse * 10 + temp % 10;
+            temp /= 10;
+        }
+        return  x == reverse;
+    }
+};
