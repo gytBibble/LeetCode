c91c96e399c8ed2669dda00e9a277ab7e023d014
gyt <1736103647@qq.com>
2019-08-19 11:20:53 +0800

    C++ : not use the long int

--- 007. Reverse Integer/solution.h
+++ 007. Reverse Integer/solution.h
@@ -1,4 +1,4 @@
-class Solution {
+class Solution_1 { // so complex
 public:
     int reverse(int x) {
         if(x == 0) return x;
@@ -33,5 +33,38 @@ public:
         if(temp < a || temp > b)
             return 0;
         return (int) temp;
+    }
+};
+
+class Solution_2 { // simplify the Solution_1
+public:
+    int reverse(int x) {
+        long res = 0;
+        while(x) {
+            res = res * 10 + x % 10;
+            x /= 10;
+            if(res > INT_MAX || res < INT_MIN)
+                return 0;
+        }
+        return (int) res;
+    }
+};
+
+class Solution_3 { // not use the long int
+public:
+    int reverse(int x) {
+        if(x == INT_MIN) return 0;
+        if(x < 0)
+            return -reverse(-x);
+        int res = 0;
+        while(x) {
+            int mod = x % 10;
+            x /= 10;
+            // check res would overflow
+            if((INT_MAX - mod) / 10 < res)
+                return 0;
+            res = res * 10 + mod;
+        }
+        return res;
     }
 };
