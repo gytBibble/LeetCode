b4aa629c3286f2a1118871791da4cf2c31ce7008
gyt <1736103647@qq.com>
2019-08-20 11:11:54 +0800

    010 C++ 2

--- 010. Regular Expression Matching/solution.h
+++ 010. Regular Expression Matching/solution.h
@@ -13,3 +13,25 @@ public:
         }
     }
 };
+
+class Solution_2 {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        dp[0][0] = true;
+        // dp[i][0] = false when i > 0; so j start from 1
+        for(int i = 0; i <= m; ++i) {
+            for(int j = 1; j <= n; ++j) {
+                if(p[j - 1] == '*') {
+                    // p[0] cannot be '*' so no need check "j >= 2" here
+                    dp[i][j] = dp[i][j - 2]
+                        || (i >= 1 && dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                } else {
+                    dp[i][j] = (i >= 1) && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
