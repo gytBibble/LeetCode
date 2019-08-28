c3b41e166f3c52bb0afb917bcdeb2faeb748a88d
gyt <1736103647@qq.com>
2019-08-28 18:20:12 +0800

    021 C++ 1

---
+++ 021. Merge Two Sorted Lists/solution.h
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution_1 { //Recursive
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == nullptr) return l2;
+        else if(l2 == nullptr) return l1;
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        }
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
+        }
+        return nullptr;
+    }
+};
+
+class Solution_2 {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(l1 == NULL) return l2;
+        else if(l2 == NULL) return l1;
+        ListNode *head = NULL;
+        if(l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        }
+        else {
+            head = l2;
+            l2 = l2->next;
+        }
+        ListNode *p = head;
+        while(l1 && l2) {
+            if(l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            }
+            else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        if(l1) p->next = l1;
+        else p->next = l2;
+        return head;
+    }
+};
